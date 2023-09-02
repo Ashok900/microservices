3787c4b4e7635262629cd8d2de70de99d4a4866a
Ashok900 <48505562+Ashok900@users.noreply.github.com>
2023-09-02 12:01:35 +0530

    Update SpringOpenfeignApplication.java

--- src/main/java/com/ex/openfeign/SpringOpenfeignApplication.java
+++ src/main/java/com/ex/openfeign/SpringOpenfeignApplication.java
@@ -1,1 +1,15 @@
+package com.ex.openfeign;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class SpringOpenfeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOpenfeignApplication.class, args);
+	}
+
+}
