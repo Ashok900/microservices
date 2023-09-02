b61aa2e986eb1e4147954394a8ee2a5b3c67963f
Ashok900 <48505562+Ashok900@users.noreply.github.com>
2023-09-02 12:03:57 +0530

    Create BookClientConfig1.java

---
+++ src/main/java/com/ex/openfeign/config/BookClientConfig1.java
@@ -0,0 +1,41 @@
+package com.ex.openfeign.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.openfeign.support.FeignEncoderProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+
+import feign.Logger;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import feign.codec.ErrorDecoder;
+
+public class BookClientConfig1 {
+	
+	@Bean
+	Logger.Level feignLoggerLevel(){
+		return Logger.Level.FULL;
+	}
+	
+	@Bean
+	public BasicAuthenticationInterceptor basicAuthenticationInterceptorBookService(@Value("${book-service-client.username}") String username, @Value("${book-service-client.password}") String password) {
+		System.out.println("basicAuthenticationInterceptorBookService");
+		return new BasicAuthenticationInterceptor(username, password);
+		
+	}
+	
+	@Bean
+	public ErrorDecoder errorDecoder() {
+		 return (ErrorDecoder) new FeignEncoderProperties();
+	}
+	
+	@Bean
+	public RequestInterceptor requestInterceptor(@Value("${book-service-client.username}") String username,@Value("${book-service-client.password}") String password) {
+		return requestTemplate->{
+			requestTemplate.header("user", username);
+			requestTemplate.header("password", password);
+		};
+		
+	}
+
+}
